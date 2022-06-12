18076771b8bdc05283c8d44d1d77b225572e7d64
roetezadi <roetezadi@gmail.com>
2022-06-12 10:22:13 +0000

    new-web

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+document.querySelector('.header__moon').onclick = function() {
+    document.querySelector('body').classList.toggle('is-black');
+}
+
+document.querySelector('.header__mob-menu').onclick = function() {
+    document.querySelector('.header-menu').classList.toggle('is-active');
+}
+
+function next_tab(tab_name) {
+    var i;
+    // var x = document.getElementsByClassName("page__intro");
+    var x = document.getElementsByName("section")
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tab_name).style.display = "block";
+  }
